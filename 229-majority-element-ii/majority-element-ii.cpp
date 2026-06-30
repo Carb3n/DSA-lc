08859c6c0054c169b269fb2d6d503819b5599777
Carb3n <shivanshp2007@gmail.com>
2026-06-30 19:36:31 +0530

    Time: 2649 ms (5.06%) | Memory: 23.7 MB (63.49%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,53 +1,27 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
-        int candidate1 = 0, candidate2 = 1;
-        int count1 = 0, count2 = 0;
-
-        // Phase 1: Find candidates
-        for (int num : nums) {
-
-            if (num == candidate1)
-                count1++;
-
-            else if (num == candidate2)
-                count2++;
-
-            else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+            if(nums[i]==nums[j]) count ++;
             }
+if(count > (n/3)) {
+    bool found = false;
 
-            else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            }
-
-            else {
-                count1--;
-                count2--;
-            }
+    for(int k = 0; k < ans.size(); k++) {
+        if(ans[k] == nums[i]) {
+            found = true;
+            break;
         }
+    }
 
-        // Phase 2: Verify
-        count1 = count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1)
-                count1++;
-            else if (num == candidate2)
-                count2++;
+    if(!found)
+        ans.push_back(nums[i]);
+}
         }
-
-        vector<int> ans;
-
-        if (count1 > nums.size() / 3)
-            ans.push_back(candidate1);
-
-        if (count2 > nums.size() / 3)
-            ans.push_back(candidate2);
-
         return ans;
     }
 };
