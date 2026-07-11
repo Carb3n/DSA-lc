281f0ab01648392a30fb526ac06527e91119209f
Carb3n <shivanshp2007@gmail.com>
2026-07-11 17:57:22 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (81.56%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-int linearsearch(vector<int>&arr,int x){
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]==x) return i;
+int Binarysearch(vector<int>&arr,int x){
+    int n=arr.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;//mid changes everytime
+        if(arr[mid]==x) return mid;
+        else if(arr[mid]>x) high=mid-1;
+        else low=mid+1;
     }
-     return -1;
+    return -1;
 }
     int search(vector<int>& nums, int target) {
-        return linearsearch(nums,target);
+        return Binarysearch(nums,target);
     }
 };
