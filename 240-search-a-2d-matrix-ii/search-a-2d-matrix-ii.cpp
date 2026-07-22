8039a34054a91e38ef0c840b2fbe0e484269cb3f
Carb3n <shivanshp2007@gmail.com>
2026-07-22 18:22:48 +0530

    Time: 101 ms (11.91%) | Memory: 18.8 MB (39.33%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,11 +3,19 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
+        //FLATTENING THE MATRIX WONK WORK
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target) return true;
+        int low=0;
+        int high=m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(matrix[i][mid]==target) return true;
+            else if (matrix[i][mid]<target) {
+                 low=mid+1;
             }
+            else high=mid-1;
         }
+     }
         return false;
     }
 };
