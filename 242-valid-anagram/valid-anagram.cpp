b85a1a93af7e5963c8826f9252db714abe1b72bf
Carb3n <shivanshp2007@gmail.com>
2026-07-25 23:22:07 +0530

    Time: 8 ms (7.02%) | Memory: 9.7 MB (50.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,19 +3,10 @@ public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size())
             return false;
-    unordered_map<char,int>freq;
-        for(char ch:s){
-            freq[ch]++;
-        }
-        for(char ch:t){
-            if(freq.find(ch)==freq.end() || freq[ch]==0){
-                return false;
-            }
-            freq[ch]=freq[ch]-1;
-        }
-         return true;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
     }
-}; //freq.find(ch)==freq.end() means if ch exist in 't' string 
-//we are first mapping s string then removing frequency after finding it in t string if count reaches 0 that means its an anagram
-// /freq[ch] == 0 means:
-//There are no more occurrences of this character left to match.
+};
