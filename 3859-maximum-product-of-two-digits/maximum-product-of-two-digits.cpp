3638186c79a53c1b3a90f289572c2c546b51fefa
Carb3n <shivanshp2007@gmail.com>
2026-07-26 01:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.88%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,27 +1,9 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int temp=n;
-        int product=1;
-        int max1=INT_MIN;
-        while(temp>0){
-            int last=temp%10;
-            max1=max(max1,last);
-            temp=temp/10;
-        }
-        temp = n;
-        int max2 = INT_MIN;
-        bool removed = false;
-        while(temp>0){
-            int last=temp%10;
-            if(last==max1 && !removed){
-                removed =true;
-            }
-            else {
-                max2 = max(max2,last);
-            }
-            temp=temp/10;
-        }
-        return max1*max2;
+        string s = to_string(n);//converts the number into a string of digits.
+        sort(s.rbegin() , s.rend());//rbegin and rend
+        int maxProduct = (s[0]-'0')*(s[1]-'0');//s[0] and s[1] are the two largest digits.
+        return maxProduct;
     }
 };
