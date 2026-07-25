e6dd41fc9b9ef9e45c3bd0f16ebffacbbadb1dd0
Carb3n <shivanshp2007@gmail.com>
2026-07-25 23:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+    unordered_map<char,int>freq;
+        for(char ch:s){
+            freq[ch]++;
+        }
+        for(char ch:t){
+            if(freq.find(ch)==freq.end() || freq[ch]==0){
+                return false;
+            }
+            freq[ch]=freq[ch]-1;
+        }
+         return true;
+    }
+};
