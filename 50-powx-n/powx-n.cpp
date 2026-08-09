07b272a673e6ee98c3c013465a9d0d9ee6f1e5e7
Carb3n <shivanshp2007@gmail.com>
2026-08-10 00:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.57%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    double power(double x, long long n) {
-        if(n == 0)
-            return 1;
+    double power(double x, long n) {
+        // Base case: anything raised to 0 is 1
+        if (n == 0) return 1.0;
 
-        double half = power(x, n/2);
+        // Base case: anything raised to 1 is itself
+        if (n == 1) return x;
 
-        if(n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+        // If 'n' is even
+        if (n % 2 == 0) {
+            // Recursive call: x * x, n / 2
+            return power(x * x, n / 2);
+        }
+        // If 'n' is odd
+        // Recursive call: x * power(x, n-1)
+        return x * power(x, n - 1);
     }
-
     double myPow(double x, int n) {
         long long N = n;
 
